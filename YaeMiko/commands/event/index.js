c69f52b6d24409fe625697943d009a6c17e8161d
yangdaidayo <yangdaidayo@gmail.com>
2023-04-02 21:37:44 +0800

    modified:   YaeMiko/commands/event/index.js 	new file:   YaeMiko/commands/live/index.js

--- YaeMiko/commands/event/index.js
+++ YaeMiko/commands/event/index.js
@@ -7,14 +7,15 @@ export const command = new SlashCommandBuilder()
 export const action = async (ctx) =>{
     ctx.reply(`小傢伙，來看看現在有甚麼活動吧
 蒙德風花節:已結束
-振晶實驗(戰鬥活動):進行中
-七聖召喚熱鬥活動:即將結束
-蕈獸活動(戰棋):即將開始
-有香自西來:尚未開始
+振晶實驗(戰鬥活動):已結束
+七聖召喚熱鬥活動:已結束
+蕈獸活動(戰棋):即將結束
+有香自西來:正在進行
 地脈雙倍:尚未開始
 深淵:已更新
 Twitch掉寶:Null
 活動連結:Null
 網頁活動:Null
-活動連結:Null`)
+活動連結:Null
+前瞻直播:3/31(五) 20:00`)
 }
